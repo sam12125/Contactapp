1d6b77f1ff9a38e2ddf0e71ee500e7edd2aea740
Saiyam vaid <99545380+sam12125@users.noreply.github.com>
2023-09-17 11:22:46 +0530

    Update Topbar.js

--- frontend/src/components/Topbar.js
+++ frontend/src/components/Topbar.js
@@ -7,14 +7,6 @@ function Topbar() {
     navigate("/create");
   };
 
-  const handleUpdate = () => {
-    navigate("/update");
-  };
-
-  const handleDelete = () => {
-    navigate("/delete");
-  };
-
   const handleContact = () => {
     navigate("/");
   };
@@ -22,8 +14,6 @@ function Topbar() {
     <>
       <div>
         <button onClick={handleCreate}>Create Contacts</button>
-        <button onClick={handleUpdate}>Update Contacts</button>
-        <button onClick={handleDelete}>Delete Contacts</button>
       </div>
       <div>
         <button onClick={handleContact}>Contact List</button>
